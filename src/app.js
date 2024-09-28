cd96431c3e06e83289bc6511a3c241a4363f8bb8
Prashupsp17 <shindeprashant967@gmail.com>
2024-09-28 14:32:09 +0530

    Multiple Route Handlers

--- src/app.js
+++ src/app.js
@@ -2,18 +2,57 @@ const express = require("express");
 
 const app = express();
 
+// app.use("/route",rh1,[rh2,rh3],rh4,rh5);
+// app.get, app.post and all other we can use;
 
-app.use("/test",(req,res) => {
-    res.send("Hello from the Test !");
-})
+app.get(
+    "/user",
+    (req,res,next) => {
+        console.log("Handling the route user");
+        next();
+    },
+    (req,res,next) => {
+        console.log("Handling the route user");
+        next();
+    },
+    (req,res,next) => {
+        console.log("Handling the route user");
+        next();
+    },
+    (req,res) => {
+        console.log("Handling the route user");
+        res.send("4th Response");
+    }
 
-app.use("/hello",(req,res) => {
-    res.send("Hello Hello Hello");
-})
+)
 
-app.use("/",(req,res) => {
-    res.send("Hello from the Dashboard!");
-})
+// app.get("/user/:userId/:name/:password", (req,res) => {
+//     res.send({firstname:"Prashant",lastname:"shinde"}); 
+// })
+
+// app.get("/user", (req,res) => {
+//     res.send({firstname:"Prashant",lastname:"shinde"}); 
+// })
+
+// app.post("/user", (req,res) => {
+// // Saved to Db
+//     res.send("Data Successfully saved to the database");
+// })
+
+// app.use("/hello/2",(req,res) => {
+//     res.send("Abracadbra");
+// })
+// app.use("/test",(req,res) => {
+//     res.send("Hello from the Test !");
+// })
+
+// app.use("/hello",(req,res) => {
+//     res.send("Hello Hello Hello");
+// })
+
+// app.use("/",(req,res) => {
+//     res.send("Hello from the Dashboard!");
+// })
 
 app.listen(3000, () => {
     console.log("Server is successfully listening on port 3000...");
