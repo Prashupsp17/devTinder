fccbec3395f889fe0dd19affd034ecd914c67c2e
Prashupsp17 <shindeprashant967@gmail.com>
2024-09-30 07:53:43 +0530

    Get APIs

--- src/app.js
+++ src/app.js
@@ -30,6 +30,47 @@ try{
 
 })
 
+// For One user
+
+app.get("/user",async (req,res) => {
+    const userEmail = req.body.emailId;
+    // FindOne
+    try{
+        const user = await User.findOne({emailId:userEmail});
+        if(!user){
+            res.status(404).send("User not found");
+        }else{
+            res.send(user);
+        }
+       
+    }
+
+    // try{
+    //     const users = await User.find({emailId:userEmail})
+    //     if(users.length === 0){
+    //         res.status(404).send("User not found");
+    //     }else{
+    //         res.send(users);
+    //     }
+    // }
+    catch(err){
+        res.status(404).send("Something went wrong");
+    }
+
+})
+
+// Feed API - GET/feed get all the users from the database
+
+app.get("/feed",async (req,res) => {
+ try{
+    const users = await User.find({});
+    res.send(users);
+
+ }catch(err){
+    res.status(404).send("Something went wrong");
+ }
+});
+
 connectDB()
 .then(() => {
     console.log("Database connection established");
